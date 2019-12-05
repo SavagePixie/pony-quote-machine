8209368a62f054d0158d7087252e53518cefc966
SavagePixie <atilaeldos@hotmail.com>
2019-12-05 15:34:40 +0100

    added handler to close db connection

--- src/db.js
+++ src/db.js
@@ -1,9 +1,16 @@
 require('dotenv').config()
 const mongo = require("mongodb")
 
-module.exports = () =>
+module.exports = { closeDb, initDb }
+
+const closeDb = client => () => client.close().then(() => {
+    console.log("See you next time!")
+    process.exit()
+})
+
+const initDb = () =>
   mongo.MongoClient.connect(process.env.MONGO_URI, { useUnifiedTopology: true, useNewUrlParser: true })
     .then(client => Promise.all([
-      client,
-      client.db(process.env.MONGO_DB)
+        client,
+        client.db(process.env.MONGO_DB)
     ]))
