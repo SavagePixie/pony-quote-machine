f3acd93917055047cab7658f0a03bd68bd0eb13e
SavagePixie <atilaeldos@hotmail.com>
2019-12-06 14:31:23 +0100

    broken up lines to improve readability

--- production/react/main.js
+++ production/react/main.js
@@ -58,12 +58,29 @@ const QuoteMachine = () => {
   )
 }
 
-const Button = props => <button id='new-quote' className='btn' onClick={props.setPony}>{props.text}</button>
+const Button = props => (<button 
+    id='new-quote'
+    className='btn'
+    onClick={props.setPony}
+  >
+    {props.text}
+  </button>)
 
-const TweetQuote = props => <a className='btn' id='tweet-quote' href={`https://twitter.com/intent/tweet?hashtags=quotes&text=${encodeURIComponent(props.quote + '\nBy ' + props.pony)}`} target='_blank'><i className='fa fa-twitter'></i></a>
+const TweetQuote = props => (<a
+    className='btn'
+    id='tweet-quote'
+    href={`https://twitter.com/intent/tweet?hashtags=quotes&text=${encodeURIComponent(`${props.quote}\nBy ${props.pony}`)}`}
+    target='_blank'
+  >
+    <i className='fa fa-twitter'></i>
+  </a>)
 
-const QuoteAuthor = props => <p id='author'>&mdash; {props.pony} <img id='photo' src={props.image} /></p>
+const QuoteAuthor = props => (<p id='author'>
+    &mdash; {props.pony} <img id='photo' src={props.image} />
+  </p>)
 
-const QuoteText = props => <p id='quote'><i className='fa fa-quote-left'></i> {props.quote}</p>
+const QuoteText = props => (<p id='quote'>
+    <i className='fa fa-quote-left'></i> {props.quote}
+  </p>)
 
 ReactDOM.render(<QuoteMachine />, document.getElementById('quote-box'))
